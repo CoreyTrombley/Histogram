532b5f42401fb39b374a211aba8d789461efa920
Corey Trombley <corey@quilible.com>
2013-01-04 11:41:01 -0500

    removed http://

--- app/controllers/histogram_controller.rb
+++ app/controllers/histogram_controller.rb
@@ -2,7 +2,7 @@ class HistogramController < ApplicationController
   def search
   end
   def result
-    result = HTTParty.get("http://#{params[:url]}")
+    result = HTTParty.get("#{params[:url]}")
     answer = result.gsub(/(\W|\d)/, " ").split
     @answer = answer.inject(Hash.new(0)) { |h, x| h[x] += 1; h}.sort_by {|k,v| v.to_i }.reverse
   end
